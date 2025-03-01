7a284d1c18df3ceecfe9e7dacf8028cba64a4084
YashNetOps998 <navadiya.ya@northeastern.edu>
2025-03-01 16:56:46 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.2.0"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "viatrixHCPLabs"
     workspaces {
       name = "ace-iac-day-two"
     }
